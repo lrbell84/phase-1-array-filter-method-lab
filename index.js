564d09f00b53d3803aa3b1541c1607edd436bf28
Lydia Bell <lrbell84@gmail.com>
2021-07-12 01:26:34 +0000

    commit tree 7531e7e6262f3d52500760bd06b1e535257c275d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(origin, search) {
+    return origin.filter (viableMatch =>
+    viableMatch.toLowerCase() === search.toLowerCase()
+    )
+}
+
+function fuzzyMatch (origin, quizString) {
+    return origin.filter (viableMatch =>
+    viableMatch.toLowerCase () .indexOf(quizString.toLowerCase ()) === 0
+    )
+}// Code your solution here
+
+function matchName (origin, searchName) {
+    return origin.filter (data => data.name === searchName)
+}
